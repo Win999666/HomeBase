15f71291d257e29b169faf953004fb92098b1734
Slav <galantsev.slava999@gmail.com>
2024-05-22 21:23:45 +0300

    xzz

---
+++ ConsoleApp9/ConsoleApp9/Interface1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace ConsoleApp9
+//{
+//    internal interface Interface1
+//    {
+//        //enum Alfa
+        //{
+        //    A = 1,
+        //    B = 2,
+        //    C = 3,
+        //    D = 4,
+        //    E = 5,
+        //    F = 6,
+        //    G = 7,
+        //    H = 8,
+        //    I = 9,
+        //    J = 10,
+        //    K = 11,
+        //    L = 12,
+        //    M = 13,                
+        //    N = 14,
+        //    O = 15,
+        //    P = 16,
+        //    Q = 17,
+        //    R = 18,
+        //    S = 19,
+        //    T = 20,
+        //    U = 21,
+        //    V = 22,
+        //    W = 23,
+        //    X = 24,
+        //    Y = 25,
+        //    Z = 26,
+        //}
+
+
+    //    public string[] Alfavit(string[] str)//принимаем массив строк
+    //    {
+    //        char one;
+    //        char two;
+    //        List<string> list;
+    //         int c = str.Length;
+    //        int i = 0;
+
+    //        foreach (string item in str)//перебираем массив строк
+    //        { 
+    //            i++;//считаем операции
+    //            char[] ch = item.ToCharArray();//превращаем строки в массив чаров
+    //            one = ch[0];             
+    //        }
+    //        return
+    //    }
+    //}
+//}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+internal class Program
+{
+//    Задание: Реализация интерфейсов IComparable и IComparer
+//Описание:
+
+//Создай класс Book, представляющий книгу с такими свойствами, 
+//        как Title(название), Author(автор) и Year(год издания). Реализуй интерфейс IComparable<Book> в этом классе,
+//        чтобы по умолчанию книги можно было сортировать по названию.Также создай отдельный компаратор BookComparerByYear,
+//            реализующий интерфейс IComparer<Book>, для сортировки книг по году издания
+    private static void Main(string[] args)
+    {
+        Book beleberda = new Book("Evil Dead","Rob Zombie",1989);
+        Book beleberda2 = new Book("Tom soer", "Kventin Tatantino", 1999);  
+        Book beleberda3 = new Book("Don Kixot", "Xer Morzhovui", 1987);
+        Book beleberda4 = new Book("Titans", "Bomzh pozornui", 1979);
+        Book beleberda5= new Book("Brozers Karamazovi", "Lev Tolstoi", 1981);
+        Book beleberda6 = new Book("Fathers and kids", "Some Tyrgenew", 1948);
+        List<Book> list = new List<Book> {beleberda,beleberda2,beleberda3,beleberda4,beleberda5,beleberda6 };
+        
+        foreach (Book book in list)
+        {
+            Console.WriteLine(book.Title,book.Author,book.Year);
+        } 
+        list.Sort(new BookCompareYears());
+        foreach (var item in list)
+        {
+            Console.WriteLine(item.Year);
+        }
+    }
+}
+class Book
+
+{
+    public Book(string title, string author, int year)
+        {
+        this.Title = title;
+        this.Author = author;
+        this.Year = year;
+        }
+  public  string Title;
+   public string Author;
+   public int Year;
+
+
+}
+class BookCompareYears : IComparer<Book>
+{
+    public int Compare(Book? x, Book? y)
+    {
+        //if (x.Year==y.Year) return 0;
+       // if(x.Year>y.Year) return -1;
+        if(y.Year>x.Year) return 1;
+       else if(y.Year<x.Year) return -1;
+        else return 0;
+       
+
+    }
+}
+
